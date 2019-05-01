b61e92a53b8680dd8f01f21d28c47dae9b021ffe
Mario A <49696044+mario279@users.noreply.github.com>
2019-05-01 11:38:48 -0400

    Update appmyown.js

--- appmyown.js
+++ appmyown.js
@@ -10,7 +10,8 @@ var UIController = (function(){
 
 // weather info
 async function getWeather(){
-   fetch('https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?lat=-34.397,lon=150.644',{mode: 'no-cors'});
+   fetch('https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?lat=-34.397,lon=150.644');
+fetch('https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?lat=-34.397,lon=150.644');
  }
   .then(result =>{
     console.log(result);
