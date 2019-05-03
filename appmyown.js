13acea4a53205665b6dd721329199496256745e1
Mario A <49696044+mario279@users.noreply.github.com>
2019-05-03 05:51:35 -0400

    Update appmyown.js

--- appmyown.js
+++ appmyown.js
@@ -109,7 +109,6 @@ var controller = (function(budgetCtrl, UICtrl)
 
         var StringsDOM = UIController.getDOMStrings();
         document.querySelector(StringsDOM.inputButton).addEventListener('click', ctrlAddItem);
-
         document.addEventListener('keypress', function(event)
         {
           if (event.keycode === 13 || event.which === 13)
